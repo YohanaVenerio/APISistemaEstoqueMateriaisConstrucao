f48f04947d34d230fbd33d2413e3ceaaa532458d
Any Caroline <168498509+Anycarol12345@users.noreply.github.com>
2025-06-08 15:24:14 -0300

    .

--- demo/src/main/java/com/example/demo/config/SwaggerConfig.java
+++ demo/src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -29,7 +29,7 @@ public class SwaggerConfig implements WebMvcConfigurer {
         return openApi -> {
             openApi.getInfo().setTitle("Seminário 3 ADS/ESW"); // Renomeia o título
             openApi.getInfo().setVersion("1.0.0"); // Define a versão
-            openApi.getInfo().setDescription("API de gerenciamento de usuarios."); // Define a descrição
+            openApi.getInfo().setDescription("API Sistema de Estoque de Materiais de Construção."); // Define a descrição
         };
     }
 }
